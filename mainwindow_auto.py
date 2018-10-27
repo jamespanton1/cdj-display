6f6d52ac6f3c78780ed11f1a4ef61d590ab38b89
james.panton1 <james.panton1@hotmail.co.uk>
2018-10-27 14:04:28 +0100

    UI Resize

--- mainwindow_auto.py
+++ mainwindow_auto.py
@@ -11,11 +11,11 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 470)
+        MainWindow.resize(800, 480)
         self.centralWidget = QtWidgets.QWidget(MainWindow)
         self.centralWidget.setObjectName("centralWidget")
         self.tblMain1 = QtWidgets.QTableWidget(self.centralWidget)
-        self.tblMain1.setGeometry(QtCore.QRect(0, 0, 800, 260))
+        self.tblMain1.setGeometry(QtCore.QRect(0, 0, 800, 390))
         self.tblMain1.setEditTriggers(QtWidgets.QAbstractItemView.NoEditTriggers)
         self.tblMain1.setTabKeyNavigation(False)
         self.tblMain1.setProperty("showDropIndicator", False)
@@ -24,20 +24,12 @@ class Ui_MainWindow(object):
         self.tblMain1.setColumnCount(0)
         self.tblMain1.setRowCount(0)
         self.lblImg1 = QtWidgets.QLabel(self.centralWidget)
-        self.lblImg1.setGeometry(QtCore.QRect(0, 260, 800, 150))
+        self.lblImg1.setGeometry(QtCore.QRect(164, 390, 634, 90))
         self.lblImg1.setText("")
         self.lblImg1.setObjectName("lblImg1")
         MainWindow.setCentralWidget(self.centralWidget)
-        self.menuBar = QtWidgets.QMenuBar(MainWindow)
-        self.menuBar.setGeometry(QtCore.QRect(0, 0, 800, 22))
-        self.menuBar.setObjectName("menuBar")
-        MainWindow.setMenuBar(self.menuBar)
-        self.mainToolBar = QtWidgets.QToolBar(MainWindow)
-        self.mainToolBar.setObjectName("mainToolBar")
-        MainWindow.addToolBar(QtCore.Qt.TopToolBarArea, self.mainToolBar)
-        self.statusBar = QtWidgets.QStatusBar(MainWindow)
-        self.statusBar.setObjectName("statusBar")
-        MainWindow.setStatusBar(self.statusBar)
+        self.actionImport = QtWidgets.QAction(MainWindow)
+        self.actionImport.setObjectName("actionImport")
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
@@ -45,4 +37,5 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.actionImport.setText(_translate("MainWindow", "Import"))
 
